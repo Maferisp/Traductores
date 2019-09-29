12a215b9446c42970d1b020c50dc682f67ee8044
Maria F Machado <mafermach@gmail.com>
2019-09-29 16:38:15 -0400

    solo tiene los nombres

---
+++ Etapa1-1310780-1310787.py
@@ -0,0 +1,8 @@
+"""
+    Analizador Lexicografico del Lenguaje GuardedUSB
+    Primera fase del proyecto
+    Traductores e Interpretadores (CI-3725)
+    Maria Fernanda Magallanes (13-10787)
+    Maria Fernanda Machado (13-10780)
+    Septiembre - Diciembre 2019
+"""
